6b67079894e69cac45b852e4b5b34790ba25ef7c
ryu <duongptryu@gmail.com>
2021-12-12 14:51:31 +0700

    FD-10: Complete API login for owner restaurant

--- route.go
+++ route.go
@@ -5,6 +5,7 @@ import (
 	"fooddelivery/middleware"
 	"fooddelivery/modules/authsso/fbsso/fbssotransport/ginfbsso"
 	"fooddelivery/modules/authsso/googlesso/googlessotransport/gingooglesso"
+	"fooddelivery/modules/restaurantowner/restaurantownertransport/ginrestaurantowner"
 	"fooddelivery/modules/user/usertransport/ginuser"
 	"github.com/gin-gonic/gin"
 )
@@ -19,6 +20,7 @@ func v1Route(r *gin.Engine, appCtx component.AppContext) {
 	{
 		v1.POST("/register", ginuser.UserReigster(appCtx))
 		v1.POST("/activate", ginuser.UserActiveAccount(appCtx))
+		v1.POST("/resend-otp-active", ginuser.ResendOTPActive(appCtx))
 		v1.POST("/login", ginuser.UserLogin(appCtx))
 
 		sso := v1.Group("/sso")
@@ -28,6 +30,17 @@ func v1Route(r *gin.Engine, appCtx component.AppContext) {
 
 			sso.POST("/register-facebook", ginfbsso.UserFacebookRegister(appCtx))
 			sso.POST("/login-facebook", ginfbsso.UserFacebookRegister(appCtx))
+		}
+
+		admin := v1.Group("/admin", middleware.RequireAuthAdmin(appCtx))
+		{
+			admin.POST("/owner-restaurant", ginrestaurantowner.OwnerRestaurantRegister(appCtx))
+		}
+
+		ownerRestaurant := v1.Group("/owner-restaurant")
+		{
+			ownerRestaurant.POST("/login", ginrestaurantowner.OwnerRestaurantLogin(appCtx))
+			ownerRestaurant.POST("/active", ginrestaurantowner.OwnerRestaurantActive(appCtx))
 		}
 	}
 }
