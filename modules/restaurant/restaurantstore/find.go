d43261e0444e60c8e3f7c8683e9f9ef1307e8ed7
ryu <duongptryu@gmail.com>
2021-12-18 23:35:01 +0700

    FD-25: API update status restaurant

--- modules/restaurant/restaurantstore/find.go
+++ modules/restaurant/restaurantstore/find.go
@@ -23,3 +23,21 @@ func (s *sqlStore) FindRestaurant(ctx context.Context, condition map[string]inte
 
 	return &result, nil
 }
+
+func (s *sqlStore) FindRestaurantWithoutStatus(ctx context.Context, condition map[string]interface{}, moreKeys ...string) (*restaurantmodel.Restaurant, error) {
+	db := s.db
+
+	db = db.Where(condition)
+
+	for i := range moreKeys {
+		db = db.Preload(moreKeys[i])
+	}
+
+	var result restaurantmodel.Restaurant
+
+	if err := db.Find(&result).Error; err != nil {
+		return nil, common.ErrDB(err)
+	}
+
+	return &result, nil
+}
--- modules/restaurant/restauranttransport/ginrestaurant/update.go
+++ modules/restaurant/restauranttransport/ginrestaurant/update.go
@@ -33,3 +33,27 @@ func UpdateRestaurant(appCtx component.AppContext) gin.HandlerFunc {
 		c.JSON(200, common.NewSimpleSuccessResponse(true))
 	}
 }
+
+func UpdateRestaurantStatus(appCtx component.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			panic(common.ErrParseJson(err))
+		}
+
+		var data restaurantmodel.RestaurantUpdateStatus
+
+		if err := c.ShouldBind(&data); err != nil {
+			panic(common.ErrParseJson(err))
+		}
+
+		store := restaurantstore.NewSqlStore(appCtx.GetDatabase())
+		biz := restaurantbiz.NewUpdateRestaurantBiz(store)
+
+		if err := biz.UpdateRestaurantStatusBiz(c.Request.Context(), id, &data); err != nil {
+			panic(err)
+		}
+
+		c.JSON(200, common.NewSimpleSuccessResponse(true))
+	}
+}
