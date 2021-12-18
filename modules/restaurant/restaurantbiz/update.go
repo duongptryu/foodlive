5843b2109d6e4796e65e29f3026230a9a0ee9de9
ryu <duongptryu@gmail.com>
2021-12-18 23:01:48 +0700

    FD-21: Update restaurant

--- modules/restaurant/restaurantbiz/update.go
+++ modules/restaurant/restaurantbiz/update.go
@@ -1,1 +1,39 @@
 package restaurantbiz
+
+import (
+	"context"
+	"fooddelivery/common"
+	"fooddelivery/modules/restaurant/restaurantmodel"
+)
+
+type UpdateRestaurantStore interface {
+	FindRestaurant(ctx context.Context, condition map[string]interface{}, moreKeys ...string) (*restaurantmodel.Restaurant, error)
+	UpdateRestaurant(ctx context.Context, id int, data *restaurantmodel.RestaurantUpdate) error
+}
+
+type updateRestaurantBiz struct {
+	Store UpdateRestaurantStore
+}
+
+func NewUpdateRestaurantBiz(store UpdateRestaurantStore) *updateRestaurantBiz {
+	return &updateRestaurantBiz{store}
+}
+
+func (biz *updateRestaurantBiz) UpdateRestaurantBiz(ctx context.Context, id int, data *restaurantmodel.RestaurantUpdate) error {
+	if err := data.Validate(); err != nil {
+		return err
+	}
+
+	oldData, err := biz.Store.FindRestaurant(ctx, map[string]interface{}{"id": id})
+	if err != nil {
+		return err
+	}
+	if oldData.Id == 0 || oldData.Status == false {
+		return common.RecordNotFound
+	}
+
+	if err := biz.Store.UpdateRestaurant(ctx, id, data); err != nil {
+		return err
+	}
+	return nil
+}
