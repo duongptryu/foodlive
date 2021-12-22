1dd9ea5c478fef6cf1c33ff8ab18a5bda04973a4
ryu <duongptryu@gmail.com>
2021-12-22 21:27:26 +0700

    FD-31: API get list food of restaurant

--- modules/food/foodtransport/ginfood/list_food_of_restaurant.go
+++ modules/food/foodtransport/ginfood/list_food_of_restaurant.go
@@ -1,1 +1,38 @@
 package ginfood
+
+import (
+	"fooddelivery/common"
+	"fooddelivery/component"
+	"fooddelivery/modules/food/foodbiz"
+	"fooddelivery/modules/food/foodmodel"
+	"fooddelivery/modules/food/foodstore"
+	"fooddelivery/modules/restaurant/restaurantstore"
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+func ListFoodOfRestaurant(appCtx component.AppContext) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		var filter foodmodel.Filter
+		if err := c.ShouldBindJSON(&filter); err != nil {
+			panic(common.ErrParseJson(err))
+		}
+
+		var paging common.Paging
+		if err := c.ShouldBindJSON(&paging); err != nil {
+			panic(common.ErrParseJson(err))
+		}
+		paging.Fulfill()
+
+		foodStore := foodstore.NewSqlStore(appCtx.GetDatabase())
+		restaurantStore := restaurantstore.NewSqlStore(appCtx.GetDatabase())
+		lsitFoodBiz := foodbiz.NewListFoodOfRestaurantBiz(foodStore, restaurantStore)
+
+		result, err := lsitFoodBiz.ListFoodOfRestaurantBiz(c.Request.Context(), rId, &paging, &filter)
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(200, common.NewSuccessResponse(result, paging, filter))
+	}
+}
