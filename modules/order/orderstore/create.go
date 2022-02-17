109edb7552cdb2371eb0ee64d1af9251c32df2ac
admin <admin@MacBook-Pro-cua-admin.local>
2022-02-17 09:30:19 +0700

    FD-111: UPdate

---
+++ modules/order/orderstore/create.go
@@ -0,0 +1,16 @@
+package orderstore
+
+import (
+	"context"
+	"foodlive/common"
+	"foodlive/modules/order/ordermodel"
+)
+
+func (s *sqlStore) CreateOrder(ctx context.Context, data *ordermodel.OrderCreate) error {
+	db := s.db
+	
+	if err := db.Create(data).Error; err != nil {
+		return common.ErrDB(err)
+	} 
+	return nil
+}
